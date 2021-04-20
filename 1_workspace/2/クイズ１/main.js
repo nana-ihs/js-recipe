76a13d38917574e659b80132323a6bd7c233a3fb
nana-ihs <nana.7.koala.fish@gmail.com>
2021-04-21 03:10:18 +0900

    [クイズ（1問）]まだ途中です。

---
+++ 1_workspace/2/クイズ１/main.js
@@ -0,0 +1,6 @@
+const choice1 = document.getElementById("choice-1")
+const choice2 = document.getElementById("choice-2")
+const choice3 = document.getElementById("choice-3")
+const feedback = document.getElementById("feedback")
+
+choice1.onclick
