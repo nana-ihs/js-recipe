e8ea74f6b58d93c172d0841b023cb544624dcdc0
nana-ihs <nana.7.koala.fish@gmail.com>
2021-04-23 01:14:49 +0900

    [Trello (保存機能なし)]を最初からやり直しました

--- 1_workspace/レシピ2/Trello（保存機能なし）/main.js
+++ 1_workspace/レシピ2/Trello（保存機能なし）/main.js
@@ -2,16 +2,17 @@ const inputElement = document.getElementById("input-todo")
 const container = document.getElementById("cards-container")
 const addButton = document.getElementById("add-button")
 
+const createCard = function(text) {
+  const card = document.createElement("div")
+  card.className = "card"
+}
+
 addButton.onclick = function() {
   const card = createCard(inputElement.value)
   container.append(card)
 
   inputElement.value = ""
 
-  const createCard = function(text) {
-    const card = document.createElement("div")
-    card.className = "card"
-  }
   const todo = document.createElement("div")
   todo.className = "todo"
   todo.textContent = text
