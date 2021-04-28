c28c1b6606b79f219602c2ac10fe94b269ca4ee2
nana-ihs <nana.7.koala.fish@gmail.com>
2021-04-29 02:25:24 +0900

    カウンター発展１

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,14 +1,10 @@
-const A = function(number) {
+const genkiFunction = function(number) {
   for (let n = 1; n <= number; n++) {
-    if (n % 15 === 0) {
-      console.log("FizzBazz")
-    } else if (n % 3 === 0) {
-      console.log("Fizz")
-    } else if (n % 5 === 0) {
-      console.log("Bazz")
+    if (n % 3 === 0 || n.includes("3")) {
+      console.log(n + "!!!!!!!")
     } else {
       console.log(n)
     }
   }
 }
-A(30)
+genkiFunction(40)
