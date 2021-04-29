d5e3d5a60e3e585106924e07e9b90207184606ce
nana-ihs <nana.7.koala.fish@gmail.com>
2021-04-29 23:41:26 +0900

    □と○発展２

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,8 +1,3 @@
-const number = 100
-for (let n = 1; n <= number; n++) {
-  if (n % 3 === 0 || String(n).includes("3")) {
-    console.log("${n}!!!!!!")
-  } else {
-    console.log(n)
-  }
+const genkiFunction = function() {
+  setInterval(countUp)
 }
--- 1_workspace/1/□と○/main.js
+++ 1_workspace/1/□と○/main.js
@@ -1,6 +1,14 @@
 const figure = document.getElementById("figure")
 
 figure.onclick = function() {
-  figure.classList.toggle("rounded")
-  figure.classList.toggle("triangled")
+  if (figure.classList.contains("rounded")) {
+    figure.classList.add("triangle")
+    figure.classList.remove("square")
+    figure.classList.remove("rounded")
+  } else if (figure.classList.contains("triangle")) {
+    figure.classList.remove("triangle")
+    figure.classList.add("square")
+  } else {
+    figure.classList.add("rounded")
+  }
 }
